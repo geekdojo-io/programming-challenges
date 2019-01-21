ec48b49ba1e619a90d5eafcf5e9d783a2db90eeb
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-21 05:29:18 -0800

    Create 01212019-e-maze-in.py

---
+++ 01212019-e-maze-in.py
@@ -0,0 +1,13 @@
+# Hint 1. To get the input as string, use the following code:
+S = input()
+
+# Hint 2. You can define x and y variable, and set to (0, 0)
+x, y = 0, 0
+
+# Hint 3. Change x or y based on a direction in a for-loop.
+for c in S:
+    if c == 'L':
+        # do something
+    elif c == 'R':
+        # do something
+...
