ac66267e22e64f04af92bc0a58da42169f522bab
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-22 05:57:23 -0800

    Create 01222019-ali-and-helping-innocent-people.py

---
+++ 01222019-ali-and-helping-innocent-people.py
@@ -0,0 +1,5 @@
+# Hint 1. To get the line that contains a string, you can use the following code:
+S = input()
+
+# Hint 2. This problem has lots of special rules, making it a little difficult to use a for-loop. Also, the length of a string is always 9. 
+# So, I'd just use a simple if-else logic, and print 'invalid' or 'valid' depending on a logic.
