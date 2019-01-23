5924c3b692297506b34851fc8c2f49603ebad2d3
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-23 04:53:41 -0800

    Create 01232019_two_strings.py

---
+++ 01232019_two_strings.py
@@ -0,0 +1,12 @@
+# Hint1. You can get the input integer 'T' on the first line
+#       and string S1 and S2 in a line for each test using the following code:
+T = int(input())
+for _ in range(T):
+    S1, S2 = input().split()
+    # Your logic goes here
+    
+# Hint2. S1 and S2 are said to be identical if any of them are rearranged (purmutation).
+#        So, they are anagrams. There are several techniques to find if two strings are anagrams.
+#        Here is a resource -- https://www.geeksforgeeks.org/check-whether-two-strings-are-anagram-of-each-other/
+
+# If you have any questions, start a thread on the Today's coding challenge in the #general slack channel. Good luck!
