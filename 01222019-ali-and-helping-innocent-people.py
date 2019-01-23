8d1a68481edacb1e5beae4e425068b5d28706607
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-23 04:44:29 -0800

    Update 01222019-ali-and-helping-innocent-people.py

--- 01222019-ali-and-helping-innocent-people.py
+++ 01222019-ali-and-helping-innocent-people.py
@@ -4,3 +4,32 @@ S = input()
 # Hint 2. This problem has lots of special rules, making it a little difficult to use a for-loop. 
 # Also, the length of a string is always 9. 
 # So, I'd just use a simple if-else logic, and print 'invalid' or 'valid' depending on a logic.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# My solution:
+S = input() # Get input
+
+if S[2] in 'AEIOUY': # Vowels
+    print('invalid')
+elif (((int(S[0]) + int(S[1])) & 1 == 1) or
+    ((int(S[3]) + int(S[4])) & 1 == 1) or
+    ((int(S[4]) + int(S[5])) & 1 == 1) or
+    ((int(S[7]) + int(S[8])) & 1 == 1)):
+    print('invalid')
+else:
+    print('valid')
