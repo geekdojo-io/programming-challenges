9995a267bbee78b71ad1ca5a1beb060cf7b7307d
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-24 04:42:10 -0800

    Create 01242019_duration.py

---
+++ 01242019_duration.py
@@ -0,0 +1,8 @@
+# Hint: To get N as first line with next N lines with SH, SM, EH and EM,
+#       you can use the following code:
+T = int(input())
+for _ in range(T):
+    SH, SM, EH, EM = map(int, input().split())
+    # Your logic goes here
+    
+# Hint2 - This problem can be solved as a simple math problem dealing with hours and minutes.
