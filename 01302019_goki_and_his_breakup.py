5f5f3d9ae874194244ca8620e45d48a1c955e0bd
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-30 04:25:40 -0800

    Create 01302019_goki_and_his_breakup.py

---
+++ 01302019_goki_and_his_breakup.py
@@ -0,0 +1,49 @@
+# Question: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/tds-and-his-breakup/
+
+# Hint1: You can receive inputs for this question in the following way:
+N = int(input())
+X = int(input())
+for _ in range(N):
+    Y = int(input())
+    # Write a custom code here
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# My Solution:
+N = int(input())
+X = int(input())
+for _ in range(N):
+    Y = int(input())
+    print('YES' if X <= Y else 'NO')
