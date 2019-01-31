575f52d27242d97db5196b15615bab99566a1fbc
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-01-31 04:38:57 -0800

    Create 01312019_life_universe_everything.py

---
+++ 01312019_life_universe_everything.py
@@ -0,0 +1,38 @@
+# Hint 1. To accept the input continuously, you may use the infinite loop:
+while True:
+    n = int(input())
+    # Write custom logic here
+    
+# Hint 2. To stop processing, you can use 'break' statement.
+while True:
+    n = int(input())
+    # Write custom logic here
+    # if something happens:
+    #    break
+    # else:
+    #    do something else
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# My solution:
+while True:
+    n = int(input())
+    if n == 42:
+        break
+    print(n)
