cada2871365543e6c7f76e24413ded8aac797f51
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-02-26 14:57:59 -0800

    Create 5_desktop_stdin.py

---
+++ teamcode/fall_2018_cgs/advanced_problem_set/5_desktop_stdin.py
@@ -0,0 +1,17 @@
+# Hint
+# To repeat a string for 'n' times, you can simply multiply string by n. Example:
+>>> s = 'XXOX'
+>>> s * 2 # Repeat s by 2 times.
+XXOXXXOX
+
+# My solution:
+
+T = int(input())
+for i in range(T):
+   h, w, n = map(int, input().split())
+   ar = []
+   for _ in range(h):
+       ar.append(input())
+   for _ in range(n):
+       for s in ar:
+           print(s * n)
