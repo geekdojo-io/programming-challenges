874f81e7543cee5c3654437fee291347b3262889
GeekDojo <34008252+geekdojo-io@users.noreply.github.com>
2019-02-26 14:58:30 -0800

    Update 5_desktop_stdin.py

--- teamcode/fall_2018_cgs/advanced_problem_set/5_desktop_stdin.py
+++ teamcode/fall_2018_cgs/advanced_problem_set/5_desktop_stdin.py
@@ -2,7 +2,7 @@
 # To repeat a string for 'n' times, you can simply multiply string by n. Example:
 >>> s = 'XXOX'
 >>> s * 2 # Repeat s by 2 times.
-XXOXXXOX
+XXOXXXOX # Output of s * 2
 
 # My solution:
 
